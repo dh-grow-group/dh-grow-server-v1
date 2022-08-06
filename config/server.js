a11df2c9455c63a231eeba674382b3f639daa57c
dev-abuhasan <ahinstitutte@gmail.com>
2022-08-06 10:00:52 +0600

    test

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a931d75a1eab94e0febb85742f799268'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '075a96d0-308d-43ef-8d40-8a1d11692f26'
+};
